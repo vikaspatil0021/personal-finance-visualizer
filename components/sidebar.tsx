36396fceb1e1e0b69d2a8180b162e6c4d3d97abd
Vikas Patil <vikaspatil0021o+github@gmail.com>
2025-07-06 20:04:58 +0530

    sidebar & button ui & layout

---
+++ components/sidebar.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { Dispatch, SetStateAction } from "react";
+
+import { ChevronRight } from "lucide-react";
+
+
+export default function Sidebar({
+  tabs,
+  activeTab,
+  setActiveTab
+}: {
+  tabs: string[],
+  activeTab: string,
+  setActiveTab: Dispatch<SetStateAction<string>>
+}) {
+  return (
+    <div className="m-4 overflow-hidden rounded-lg border">
+      {
+        tabs.map((each_tab: string, idx: number) => {
+          return (
+            <div key={idx}
+              className={`flex justify-between px-3 py-2 hover:bg-[#f3f3f3] cursor-pointer ${activeTab === each_tab && "bg-[#f4f4f4]"} ${idx !== 2 && "border-b"}`}
+              onClick={() => setActiveTab(each_tab)}
+            >
+              {each_tab}
+              <ChevronRight className="h-5" />
+            </div>
+          )
+        })
+      }
+    </div>
+  );
+}
